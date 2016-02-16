458cbf7fae78aa5ac70064f3be8ae57bf0af38af
Srujana <srujima@users.noreply.github.com>
2016-02-16 10:28:38 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-$(function() {
-$('#page2').click(function() {
-    $('#about').show();
-    $('#home, #page4').hide();
+$("div").each(function(){
+     $(this).hide();
+    if($(this).attr('id') == 'home') {
+        $(this).show();
+    }
 });
+$('a').on( "click", function(e) {
+    e.preventDefault();
+    var id = $(this).attr('data-related'); 
+    $("div").each(function(){
+        $(this).hide();
+        if($(this).attr('id') == id) {
+            $(this).show();
+        }
+    });
 });
