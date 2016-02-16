cee5bdedd2e711663155ad93c9e2dd74a608c7f2
Srujana <srujima@users.noreply.github.com>
2016-02-16 12:35:10 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-$('.nav a').click(function() {
-  $('.pages').hide();
-  var index = $('.nav a').index(this);
-  console.log(index);
-  $('.pages').eq(index).show();
-})
+$('.nav a').on('click', function(e) {
+    e.preventDefault();
+    var index = $('a').index(this) + 1;
+    
+    $('div').each(function(){
+        
+        if($(this).attr('rel') == index){
+            $(this).addClass('active');
+            $(this).show();
+        }
+        else{
+            $(this).removeClass('active');
+            $(this).hide();
+        }
+    });
+    
+});
