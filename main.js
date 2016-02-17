9a3a7f701c29d1b3eead57d7ce0e381f5f281a68
Srujana <srujima@users.noreply.github.com>
2016-02-17 15:00:21 -0800

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
   $(this.getAttribute('href')).show()
 });
 $("#details1").click(function () {
- $('#slide1').slideToggle({direction:"right"}, 300);
+ $('#slide1').css({
+      'width': $('#slide1').width(),
+      'height': $('#slide1').height()
+   });
+   $('#slide1').animate({'width': 'toggle'});
 });
 $("#details2").click(function () {
  $('#slide2').slideToggle({direction: "down"}, 300);
- $('#slide1').hide();
+ $('#slide1').fadeOut();
 });
 });
