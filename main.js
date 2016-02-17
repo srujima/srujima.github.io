fdfb8f5e2562df8fbdb2680b6ea60df241ffaa0a
Srujana <srujima@users.noreply.github.com>
2016-02-17 15:03:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ $("#details1").click(function () {
       'width': $('#slide1').width(),
       'height': $('#slide1').height()
    });
-   $('#slide1').animate({'width': 'toggle'});
+   $('slide2').fadeOut();
 });
 $("#details2").click(function () {
  $('#slide2').slideToggle({direction: "down"}, 300);
