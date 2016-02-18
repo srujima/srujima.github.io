3cc49b7449d25aad6a7552f15d1a91a78c75456e
Srujana <srujima@users.noreply.github.com>
2016-02-18 11:19:08 -0800

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,7 @@ $("#details2").click(function () {
  $('#slide2').slideToggle({direction: "down"}, 300);
  $('#slide1').fadeOut();
 });
+ $(".navbar-nav li a").click(function() {
+          $(".navbar-collapse").collapse('hide');
+        });
 });
