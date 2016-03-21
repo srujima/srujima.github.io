a10297b877cb0a832797921c648f714355e8f4d8
Srujana <srujima@users.noreply.github.com>
2016-03-21 16:26:17 -0700

    Create clac.js

---
+++ Calculator/clac.js
@@ -0,0 +1,29 @@
+function btnadd()
+    {
+        document.Calculator.display.value += "+";
+    }
+function btnsub()
+    {
+        document.Calculator.display.value += "-";
+    }
+function btnmulty()
+    {
+        document.Calculator.display.value += "*";
+    }
+function btnmod()
+    {
+        document.Calculator.display.value += "%";
+    }
+function btndeci()
+    {
+        document.Calculator.display.value += ".";
+    }
+function btndiv()
+    {
+        document.Calculator.display.value += "/";
+    }
+
+   function btnclear()
+    {
+   document.Calculator.display.value = "";
+    }
